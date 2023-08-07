55d9cf4ed2204676f5b92375fa98a200dc3a0b17
d4nte46 <62292852+d4nte46@users.noreply.github.com>
2023-08-07 19:44:17 -0400

    User Input Card Image Showing

--- src/main/java/com/ppptcg/POKEMONTCG/Controller/CardSuccessController.java
+++ src/main/java/com/ppptcg/POKEMONTCG/Controller/CardSuccessController.java
@@ -3,6 +3,7 @@ package com.ppptcg.POKEMONTCG.Controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.ppptcg.POKEMONTCG.DAO.PackageDao;
+import com.ppptcg.POKEMONTCG.model.CardsetEntity;
 import com.ppptcg.POKEMONTCG.model.userCardInputEntity;
 import com.ppptcg.POKEMONTCG.nonSpringclasses.PokeapiPOJO;
 import com.ppptcg.POKEMONTCG.nonSpringclasses.ptcg_io_interaction;
@@ -12,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.Optional;
+
 @Controller
 public class  CardSuccessController {
     @Autowired
@@ -20,12 +23,25 @@ public class  CardSuccessController {
     PackageDao PackageRep;
     @GetMapping("/addcardsuccess")
     public String cardsuccess(@ModelAttribute("UCIE") userCardInputEntity UCIE, Model model) throws UnirestException, JsonProcessingException {
-        model.addAttribute("UCIE",UCIE);
         ptcg_io_interaction ption = new ptcg_io_interaction(pokeapiPOJO.getApi_Key());
-        String link = ption.get_card_img(UCIE.getSetId() + "-" + UCIE.getId()).replace("\"" , "") ;
-        model.addAttribute("link",link);
-        return "cardaddsuccess";
+        CardsetEntity cardset = PackageRep.findByName(UCIE.getSetId());
+        System.out.println(cardset.getId() + UCIE.getId());
+        if(cardset.getCardset().contains("Trainer Gallery")){
+            if (UCIE.getId()<10){
+                String link = ption.get_card_img(cardset.getId() + "-TG0" + UCIE.getId()).replace("\"" , "") ;
+                model.addAttribute("link",link);
+                return "cardaddsuccess";}
+            else{
+                String link = ption.get_card_img(cardset.getId() + "-TG" + UCIE.getId()).replace("\"" , "") ;
+                model.addAttribute("link",link);
+                return "cardaddsuccess";
+            }}
+        else{
+            String link = ption.get_card_img(cardset.getId() + "-" + UCIE.getId()).replace("\"" , "") ;
+            model.addAttribute("link",link);
+            return "cardaddsuccess";}
     }
+
 
 
 }
--- src/main/java/com/ppptcg/POKEMONTCG/DAO/PackageDao.java
+++ src/main/java/com/ppptcg/POKEMONTCG/DAO/PackageDao.java
@@ -2,10 +2,15 @@ package com.ppptcg.POKEMONTCG.DAO;
 
 import com.ppptcg.POKEMONTCG.model.CardsetEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 @Repository
 public interface PackageDao extends JpaRepository<CardsetEntity,Integer> {
     List<CardsetEntity> findAll();
+    @Query("SELECT cd FROM CardsetEntity cd WHERE cd.cardset = :name")
+    CardsetEntity findByName(String name);
+
 }
