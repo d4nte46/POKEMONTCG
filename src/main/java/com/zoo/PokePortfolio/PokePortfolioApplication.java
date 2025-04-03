fa9842daa81f374b5e9fb2abf028037bab209fe6
d4nte46 <zuhayerajmain@gmail.com>
2025-04-03 00:42:11 -0400

    Spring initializr built

---
+++ src/main/java/com/zoo/PokePortfolio/PokePortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.zoo.PokePortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokePortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoo/PokePortfolio/PokePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoo.PokePortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokePortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
